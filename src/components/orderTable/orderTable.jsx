6d8ba03eed9254d3c17b95d5cca6258885446f16
Chaseb78 <chaseb78@gmail.com>
2022-02-14 13:37:48 -0500

    Added list of orders

--- src/components/orderTable/orderTable.jsx
+++ src/components/orderTable/orderTable.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
-const OrderTable = () => (
+const OrderTable = (props) => (
   <table>
     <thead>
       <tr>
         <th>Name</th>
-        <th>Order Details</th>
+        <th>Order </th>
         <th>Actions</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>Name data</td>
-        <td>Order data</td>
-        <td>
-          <button className="button muted-button">Edit</button>
-          <button className="button muted-button">Delete</button>
-        </td>
-      </tr>
+      {props.orders.length > 0 ? (
+        props.orders.map((order) => (
+          <tr key={order.id}>
+            <td> {order.name} </td>
+            <td> {order.orderInfo} </td>
+            <td>  <button className="button muted-button">Edit</button>
+          <button className="button muted-button">Delete</button> </td> </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}> No Orders </td></tr>
+      )}
     </tbody>
   </table>
 );
--- src/containers/orderAppContainer/orderAppContainer.jsx
+++ src/containers/orderAppContainer/orderAppContainer.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './orderAppContainer.css';
 import OrderTable from '../../components/orderTable';
 
 const OrderApp = () => {
-  const orderData = [];
+  const ordersData = [
+    {
+      id: 1,
+      name: 'Chase',
+      orderInfo: 'A small Choclate ice cream with sprinkles.',
+    },
+    {
+      id: 2,
+      name: 'Billy',
+      orderInfo: 'A large Strawberry ice cream with nuts.',
+    },
+    {
+      id: 3,
+      name: 'Jake',
+      orderInfo: 'A small Vanilla ice cream with Oreos.',
+    },
+  ];
+
+  const [orders, setOrders] = useState(ordersData);
+
+  const addOrder = () => {
+    order.id = order.length + 1 
+    setOrders([...orders, order])
+  }
 
   return (
     <div className="container">
@@ -11,10 +34,11 @@ const OrderApp = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add order</h2>
+          
         </div>
         <div className="flex-large">
           <h2>View orders</h2>
-          <OrderTable />
+          <OrderTable orders={orders} />
         </div>
       </div>
     </div>
